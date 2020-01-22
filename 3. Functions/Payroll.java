0f28750d07eab5ddc5f38e209054c78036f45769
Arthur <arthur.volant-ext@vinci.com>
2020-01-22 09:16:25 +0100

    Added correction

--- 3. Functions/Payroll.java
+++ 3. Functions/Payroll.java
@@ -1,20 +1,36 @@
-// Payroll.java file from Clean Code book
+// Payroll.java file from Clean Code book - CORRECTION
 
-public Money calculatePay(Employee e) throws InvalidEmployeeType{
-  switch (e.type) {
-    case COMISSIONED:
-      return calculateCommissionedPay(e)
+public abstract class Employee {
+  public abstract boolean isPayDay();
+  public abstract Money calculatePay();
+  public abstract void deliverPay(Money pay);
+}
+
+// --------------------------
+
+public interface EmployeeFactory {
+  public Employee makeEmployee(EmployeeRecord r) throws InvalidEmployeeType;
+}
+
+// --------------------------
+
+public class EmployeeFactoryImpl implements EmployeeFactory {
+  public Employee makeEmployee(EmployeeRecord r) throws InvalidEmployeeType {
+    switch (r.type) {
+      case COMISSIONED:
+      return CommissionedEmployee(e)
       break;
     
-    case HOURLY:
-    return calculateHourlyPay(e)
-    break;
+      case HOURLY:
+      return HourlyEmployee(e)
+      break;
 
-    case SALARIED:
-      return calculateSalariedPay(e)
-      break;
-      
-    default:
-      break;
+      case SALARIED:
+        return SalariedEmployee(e)
+        break;
+        
+      default:
+        throw new InvalidEmployeeType(r.type);
+    }
   }
-}
+}    
