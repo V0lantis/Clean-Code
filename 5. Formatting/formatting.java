dfb47c284af1a271ad8f9587dc449d3e515bce47
Arthur <arthur.volant-ext@vinci.com>
2020-01-22 14:01:50 +0100

    Correction of Instance variables section

--- 5. Formatting/formatting.java
+++ 5. Formatting/formatting.java
@@ -12,3 +12,42 @@ public class ReporterConfig {
     m_properties.add(property);
  }
 }
+
+// --------- Instance variables ---------
+
+// CORRECTION
+
+public class TestSuite implements Test {
+  private String fName;
+  private Vector <Test> fTests = new Vector <Test>(10);
+  
+  static public Test createTest(Class << ? extends TestCase > theClass,
+      String name) {
+       //...
+  }
+
+  public static Constructor <? extends TestCase >
+  getTestConstructor(Class << ? extends TestCase > theClass)
+  throws NoSuchMethodException {  
+    //...
+  }
+
+  public static Test warning(final String message) {  
+    //...
+  }
+
+  private static String exceptionToString(Throwable t) {  
+    //...
+  }
+
+  public TestSuite() {}
+
+  public TestSuite(final Class << ? extends TestCase > theClass) {  
+    //...
+  }
+
+  public TestSuite(Class << ? extends TestCase > theClass, String name){  
+    //...
+  } 
+  //...............
+}
