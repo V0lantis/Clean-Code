ca7eaf118a9d66adf4a084d12b1cd13371f68390
Arthur <arthur.volant-ext@vinci.com>
2020-01-22 13:49:51 +0100

    Added correction to the Vertical Density section

--- 5. Formatting/formatting.java
+++ 5. Formatting/formatting.java
@@ -2,18 +2,13 @@
 
 // --------- Vertical Density ---------
 
-// TODO: Refactore in order to increase density and readability
+// CORRECTION
 
 public class ReporterConfig {
-  /**
-  * The class name of the reporter listener
-  */
   private String m_className;
-  /**
-  * The properties of the reporter listener
-  */
   private List<Property> m_properties = new ArrayList<Property>();
  
   public void addProperty(Property property) {
     m_properties.add(property);
  }
+}
