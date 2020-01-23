8907ad5250a2ff828b2089c08ce4a59ba90258aa
Arthur <arthur.volant-ext@vinci.com>
2020-01-23 11:28:31 +0100

    Added a link to a Cheat sheet repository of the chapter. Also Added a small appplication

---
+++ 6. Objects_and_Data_Structures/objects_and_Data_Structures.java
@@ -0,0 +1,8 @@
+// objects_and_Data_Structures.java file
+
+// What better implementation of the following class we could think of?
+
+public interface Vehicule {
+  double getFuelTankCapacityInGallons();
+  double getGallonsOfGasoline();
+}
