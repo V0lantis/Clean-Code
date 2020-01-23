17f30aa475d7054eb61eccdd49f21cfc0f2e1500
Arthur <arthur.volant-ext@vinci.com>
2020-01-23 11:31:43 +0100

    Added correction to objects_and_Data_Structures.java file's application

--- 6. Objects_and_Data_Structures/objects_and_Data_Structures.java
+++ 6. Objects_and_Data_Structures/objects_and_Data_Structures.java
@@ -1,8 +1,10 @@
 // objects_and_Data_Structures.java file
 
-// What better implementation of the following class we could think of?
+// What better implementation of the following class we could think of? CORRECTION
 
 public interface Vehicule {
-  double getFuelTankCapacityInGallons();
-  double getGallonsOfGasoline();
-}
+  double getPercentFuelRemaining();
+} 
+
+// Abstract vehicle doesn't expose the details of the data.
+// The worst option is to blithely add getters and setters
