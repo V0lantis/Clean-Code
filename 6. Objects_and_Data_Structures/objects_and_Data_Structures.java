1cf78f6621401a7e5a956d8b87acee7d2c9a9f74
Arthur <arthur.volant-ext@vinci.com>
2020-01-23 11:43:51 +0100

    Adding correction of the Law of Demeter

--- 6. Objects_and_Data_Structures/objects_and_Data_Structures.java
+++ 6. Objects_and_Data_Structures/objects_and_Data_Structures.java
@@ -8,3 +8,10 @@ public interface Vehicule {
 
 // Abstract vehicle doesn't expose the details of the data.
 // The worst option is to blithely add getters and setters
+
+// Law of Demeter : Refactore the following code in order to respect the Law of Demeter:
+// CORRECTION 
+
+Options opts = ctxt.getOptions();
+File scratchDir = opts.getScratchDir();
+final string outputDir = scratchDir.getAbsolutePath();
